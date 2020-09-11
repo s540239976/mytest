1524dcfed0f3ab577fad4f8f43dab13653fae3c3
mokgadi <mokgadikutumela02@gmail.com>
2020-09-11 11:19:16 +0800

    新建项目

---
+++ src/com/hisoft/mytest.java
@@ -0,0 +1,9 @@
+package com.hisoft;
+
+public class mytest {
+    public static void main(String[] args) {
+        int a = 1, b = 2, c;
+        c = a + b;
+        System.out.println(c);
+    }
+}
