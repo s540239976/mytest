1d205372b3c04591b0e69dd82a55725545efa14d
mokgadi <mokgadikutumela02@gmail.com>
2020-09-11 11:26:48 +0800

    修改+1

--- src/com/hisoft/mytest.java
+++ src/com/hisoft/mytest.java
@@ -2,9 +2,8 @@ package com.hisoft;
 
 public class mytest {
     public static void main(String[] args) {
-        int a = 1, b = 2, c;
+        int a = 1, b = 2, c=2;
         c = a + b;
-        c = c/3;
         System.out.println(c);
     }
 }
