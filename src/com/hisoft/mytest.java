12f3e54de9034cd352753d8d5c230096252bef6b
s540239976 <71064095+s540239976@users.noreply.github.com>
2020-09-11 11:24:23 +0800

    Update mytest.java

--- src/com/hisoft/mytest.java
+++ src/com/hisoft/mytest.java
@@ -4,7 +4,7 @@ public class mytest {
     public static void main(String[] args) {
         int a = 1, b = 2, c;
         c = a + b;
-        c = c%3;
+        c = c/3;
         System.out.println(c);
     }
 }
